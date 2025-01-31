6db86fc2d9ec7046bd22505afa35624c490c3145
piauski <piauskiii@gmail.com>
2025-01-31 23:34:47 +0000

    Add outline shader

--- scripts/game.gd
+++ scripts/game.gd
@@ -8,6 +8,8 @@ signal player_added(Node3D)
 
 @export var player_scene: PackedScene
 
+@export var stencil_viewport: SubViewport
+@export var stencil_camera: Camera3D
 
 func _ready() -> void:
 	# Preconfigure game.
@@ -45,3 +47,14 @@ func start_game() -> void:
 		player.set_board_rotation.rpc_id(peer_id, spawn.rotation.y + PI / 2)
 		i += 1
 	
+
+func _process(_delta: float) -> void:
+	var viewport := get_viewport()
+	var current_camera := viewport.get_camera_3d()
+	
+	if stencil_viewport.size != viewport.size:
+		stencil_viewport.size = viewport.size
+		
+	if current_camera:
+		stencil_camera.fov = current_camera.fov
+		stencil_camera.global_transform = current_camera.global_transform
